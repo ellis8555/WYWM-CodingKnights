64c488640caa36fefbd323ab3bb8857f08ccd3ca
ellis <lsp1@hotmail.com>
2023-01-16 11:01:20 -0700

    reg expression edit

--- resources/validations/threeNames.py
+++ resources/validations/threeNames.py
@@ -1,1 +1,1 @@
-threeNames = "^[a-zA-Z]+[\.-]?[\s]?[a-zA-Z-]*[\.-]?[\s]?[a-zA-Z]*[\.-]?[\s]?[a-zA-Z]*$"
+threeNames = "^([a-zA-Z]+(([-][a-zA-Z]+)|(\.\s[a-zA-Z]+))?)(\s[a-zA-Z]+([-][a-zA-Z]+)?){0,2}$"
